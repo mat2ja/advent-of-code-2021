e690e2bed5570ff03e49ad2179ef95688d7be997
Matija Osrečki <matija.osrecki@gmail.com>
2021-12-01 09:45:25 +0100

    Complete day 1 :sparkles:_

---
+++ day1/day1-1.py
@@ -0,0 +1,17 @@
+'''
+Count the number of times a depth measurement increases from the previous measurement. (There is no measurement before the first measurement.)
+'''
+
+f = open(r'day1/input.txt', 'r')
+rows = [int(row[:-1]) for row in f]
+
+
+def count_increases(rows):
+    count = 0
+    for i in range(1, len(rows)):
+        if (rows[i] > rows[i-1]):
+            count += 1
+    return count
+
+
+print(count_increases(rows))  # 1552
---
+++ day1/day1-2.py
@@ -0,0 +1,38 @@
+'''
+Start by comparing the first and second three-measurement windows. The measurements in the first window are marked A (199, 200, 208); their sum is 199 + 200 + 208 = 607. The second window is marked B (200, 208, 210); its sum is 618. The sum of measurements in the second window is larger than the sum of the first, so this first comparison increased.
+
+Your goal now is to count the number of times the sum of measurements in this sliding window increases from the previous sum. So, compare A with B, then compare B with C, then C with D, and so on. Stop when there aren't enough measurements left to create a new three-measurement sum.
+'''
+
+f = open(r'day1/input.txt', 'r')
+rows = [int(row[:-1]) for row in f]
+
+
+def count_increases_triples(rows):
+    count = 0
+    for i in range(3, len(rows)):
+
+        sum_prev = sum([rows[i-1], rows[i-2], rows[i-3]])
+        sum_curr = sum([rows[i], rows[i-1], rows[i-2]])
+
+        if (sum_curr > sum_prev):
+            count += 1
+    return count
+
+
+print(count_increases_triples(rows))  # 1597
+
+# test = [
+#     199,
+#     200,
+#     208,
+#     210,
+#     200,
+#     207,
+#     240,
+#     269,
+#     260,
+#     263,
+# ]
+
+# print(count_increases_triples(test)) # 5
